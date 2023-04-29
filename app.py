fedad8f7cbb8a7b4f89754cd6f7b1ea020f7f525
lvespitia <lvespitias@udistrital.edu.co>
2023-04-29 07:48:54 -0500

    First code :airplane:

---
+++ app.py
@@ -0,0 +1,8 @@
+#Importar backend
+from backend.areas import *
+
+print("Prueba")
+
+areacirculo(100)
+
+areacuadrado(100)
---
+++ backend/areas.py
@@ -0,0 +1,11 @@
+"""
+    Cálculo de diferentes áreas
+"""
+
+import math
+
+def areacirculo(radio):
+    print(math.pi*radio**2)
+
+def areacuadrado(lado):
+    print(lado*lado)
